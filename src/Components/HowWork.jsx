cb49215c0300b619ecec2e457e5c0b4f8555891b
ismailcet <ismailcet3@gmail.com>
2022-10-20 22:38:39 +0300

    HowWork is added.Style is not finished.

--- src/Components/HowWork.jsx
+++ src/Components/HowWork.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HowWork = () => {
-  return <section className="howWork mt-40 w-max[100%]">HowWork</section>;
+  return (
+    <section className="howWork mt-40 xl:mx-[-16rem] lg:mx-[-8rem] h-[70vh] bg-mainColor bg-workPattern bg-no-repeat bg-center bg-cover">
+      HowWork
+    </section>
+  );
 };
 
 export default HowWork;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        workPattern: "url('./assets/patternBG.png')",
+      },
       colors: {
         main: "#000000",
         textColor: "#02073E",
