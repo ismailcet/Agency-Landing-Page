8f9c56f482b761a957d580b68b38b681a8a179b7
ismailcet <ismailcet3@gmail.com>
2022-10-25 00:06:04 +0300

    Blog Component is added.

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Blog = () => {
-  return <section className="mt-40">Blog</section>;
+  return (
+    <section className="mt-40">
+      <div className="blog-up">
+        <p className="team-info uppercase font-semibold text-[14px] text-mainColor mb-2">
+          Our Blog
+        </p>
+        <h3 className="team-title text-[2rem] font-bold">
+          Explore our products for your business solution
+        </h3>
+      </div>
+      <div className="blog-down">
+        <div className="blog"></div>
+        <div className="blog"></div>
+        <div className="blog"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
